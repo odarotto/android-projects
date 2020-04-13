0a5b7b76cb83151ab5205f15d86076f9068cdb1c
odarotto <odarotto@gmail.com>
2020-04-13 16:20:42 -0400

    Challenge01 completed

--- GeoQuiz/app/src/main/java/android/bignerdranch/com/QuizActivity.java
+++ GeoQuiz/app/src/main/java/android/bignerdranch/com/QuizActivity.java
@@ -2,9 +2,12 @@ package android.bignerdranch.com;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.util.Queue;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -20,9 +23,12 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast t = Toast.makeText(QuizActivity.this,
                                 R.string.incorrect_toast,
-                                Toast.LENGTH_SHORT).show();
+                                Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP, 0, 0);
+                t.show();
+
             }
         });
 
@@ -30,9 +36,11 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this,
+                Toast t = Toast.makeText(QuizActivity.this,
                                 R.string.incorrect_toast,
-                                Toast.LENGTH_SHORT).show();
+                                Toast.LENGTH_SHORT);
+                t.setGravity(Gravity.TOP, 0, 0);
+                t.show();
             }
         });
     }
